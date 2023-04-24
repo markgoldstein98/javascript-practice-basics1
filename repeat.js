e7f92595f50bd2c0e7088ed551d98c2054079c9a
Mark Abraham Goldstein <123721646+markgoldstein98@users.noreply.github.com>
2023-04-24 20:11:22 +0200

    solved number 5 and 6 task

--- repeat.js
+++ repeat.js
@@ -1,4 +1,12 @@
 // add whatever parameters you deem necessary - good luck!
-function repeat(){
+function repeat(string, num){
+    let stringNum= '';
 
+    for (let index = 0; index < num; index++) { // nem adjuk meg neki hányszor fussuk,
+                                                // num tárolja ezt a paramétert 
+            stringNum += string
+            
+    }
+    return stringNum;
 }
+console.log(repeat('Apple',3))
--- stringLastIndexOf.js
+++ stringLastIndexOf.js
@@ -1,4 +1,14 @@
 // add whatever parameters you deem necessary - good luck!
-function stringLastIndexOf(){
-
+function stringLastIndexOf(words, char){
+    let last = -1
+    
+    for (let index = 0; index < words.length; index++) {
+        if (words[index] === char) {
+            last=index // nekünk az kell hogy hanyadik indexen van
+        }
+        
+        
+    } return last
 }
+
+console.log(stringLastIndexOf('elephant','e'))
